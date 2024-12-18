8b171abba713b35272366cf52e82a35c13c61ac1
DiaczukMatias <diaczukm@gmail.com>
2024-12-18 20:48:34 -0300

    cambios rockets

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,21 @@
+import { useRouter } from "next/navigation";
 import styles from "./Navbar.module.css";
 import Image from "next/image";
 
 const Navbar = () => {
+  const router = useRouter(); // Hook para manejar la navegación
+
+  const handleLogoClick = () => {
+    router.push("/"); // Redirigir a la landing page
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logoContainer}>
+      <div
+        className={styles.logoContainer}
+        onClick={handleLogoClick}
+        style={{ cursor: "pointer" }}
+      >
         <Image src="/logoo.png" alt="Logo" width={190} height={190} />
       </div>
       <ul className={styles.navLinks}>
@@ -14,18 +25,8 @@ const Navbar = () => {
           </a>
         </li>
         <li>
-          <a href="/launches">
-            <p>LAUNCHES</p>
-          </a>
-        </li>
-        <li>
-          <a href="/rockets">
-            <p>ROCKETS</p>
-          </a>
-        </li>
-        <li>
-          <a href="/rockets">
-            <p>ROCKETS</p>
+          <a href="/home">
+            <p>HOME</p>
           </a>
         </li>
       </ul>
--- src/Views/RocketsView/RocketsView.tsx
+++ src/Views/RocketsView/RocketsView.tsx
@@ -17,6 +17,23 @@ const RocketsView: React.FC = () => {
   const [rockets, setRockets] = useState<Rocket[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedRocket, setSelectedRocket] = useState<Rocket | null>(null);
+
+  // Función para mapear los nombres de los cohetes a sus imágenes
+  const getRocketImage = (rocketName: string): string => {
+    switch (rocketName.toUpperCase()) {
+      case "FALCON 1":
+        return "/FALCON1.png";
+      case "FALCON 9":
+        return "/FALCON9.png";
+      case "STARSHIP":
+        return "/STARSHIP.png";
+      case "FALCON HEAVY":
+        return "/FALCONHEAVY.png";
+      default:
+        return "/rockets/default.png";
+    }
+  };
 
   useEffect(() => {
     const fetchRockets = async () => {
@@ -41,30 +58,58 @@ const RocketsView: React.FC = () => {
     fetchRockets();
   }, []);
 
+  const handleRocketClick = (rocket: Rocket) => {
+    setSelectedRocket(rocket);
+  };
+
   if (loading) return <p className={styles.loading}>Loading...</p>;
   if (error) return <p className={styles.error}>{error}</p>;
 
   return (
     <div className={styles.container}>
       <Navbar />
-      <h1 className={styles.title}>SpaceX Rockets</h1>
+      <h1 className={styles.title}>SPACEX ROCKETS</h1>
+
+      {/* Mostrar todas las tarjetas */}
       <div className={styles.rocketsContainer}>
         {rockets.map((rocket) => (
-          <div key={rocket.rocket_id} className={styles.rocketCard}>
-            <h2 className={styles.rocketName}>{rocket.rocket_name}</h2>
-            <p className={styles.description}>{rocket.description}</p>
-            <p>
-              <strong>First Flight:</strong> {rocket.first_flight}
-            </p>
-            <p>
-              <strong>Country:</strong> {rocket.country}
-            </p>
-            <p>
-              <strong>Company:</strong> {rocket.company}
-            </p>
-          </div>
+          <button
+            key={rocket.rocket_id}
+            className={styles.button}
+            onClick={() => handleRocketClick(rocket)}
+          >
+            <div className={styles.rocketInfo}>
+              <h2 className={styles.rocketName}>{rocket.rocket_name}</h2>
+            </div>
+          </button>
         ))}
       </div>
+
+      {/* Mostrar información seleccionada */}
+      {selectedRocket && (
+        <div className={styles.selectedRocket}>
+          <div className={styles.selectedInfo}>
+            <h2 className={styles.rocketName}>{selectedRocket.rocket_name}</h2>
+            <p className={styles.description}>{selectedRocket.description}</p>
+            <p className={styles.Flight}>
+              <strong>First Flight:</strong> {selectedRocket.first_flight}
+            </p>
+            <p className={styles.country}>
+              <strong>Country:</strong> {selectedRocket.country}
+            </p>
+            <p className={styles.company}>
+              <strong>Company:</strong> {selectedRocket.company}
+            </p>
+          </div>
+          <div className={styles.selectedImage}>
+            <img
+              src={getRocketImage(selectedRocket.rocket_name)} // Obtener la imagen correspondiente
+              alt={selectedRocket.rocket_name}
+              className={styles.rocketImage}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
